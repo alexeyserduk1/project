0b9d35720991028c2a2807a37b7c5ae1f94355f3
alexeyserduk1 <alexeyserduk1@mail.ru>
2014-04-30 15:32:58 +0300

    q1

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Work
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-    config.assets.initialize_on_precompile = false
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
@@ -52,7 +52,7 @@ module Work
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-
+    config.assets.initialize_on_precompile = false
     # Enable the asset pipeline
     config.assets.enabled = true
 
